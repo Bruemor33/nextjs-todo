b0a70d371f9e78e5d323c14a872883fac43ae122
arymer <arymer1@gmail.com>
2022-04-27 10:12:43 -0400

    Updates

---
+++ pages/api/list.js
@@ -0,0 +1,15 @@
+import { db_connections } from "../../project.config"
+
+export default async (req, res) => {
+    const token = db_connections.token;
+    const url = `${db_connections.url}/lrange/todo/0/100?_token=${token}`;
+
+    return fetch(url)
+        .then(r => r.json())
+        .then(data => {
+            let result = JSON.stringify(data.result)
+            console.log("raw_data", data)
+            console.log("result", result)
+            return res.status(200).json(result)
+        })
+}
